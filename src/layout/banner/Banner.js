a65c5aa36abc510bf826a417209915121ac1a10a
Guilherme <guibbs11032003@gmail.com>
2025-01-13 19:59:02 -0300

    att: banner animation

--- src/layout/banner/Banner.js
+++ src/layout/banner/Banner.js
@@ -1,10 +1,23 @@
+import { useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 import styles from './Banner.module.scss';
 
 export const Banner = () => {
+    const bannerNameRef = useRef(null);
+
+    useGSAP(() => {
+        gsap.fromTo(
+            bannerNameRef.current,
+            { opacity: 0 },
+            { opacity: 1, duration: 1, ease: "power1.in" }
+        );
+    }, [])
+
     return (
         <section className={styles.banner_container}>
             <div className={styles.banner_background}>
-                <div className={styles.banner_name}>
+                <div ref={bannerNameRef} className={styles.banner_name}>
                     <span className={styles.banner_name_first}>
                         Luana
                     </span>
