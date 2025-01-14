f34fe845cdfca4bd2109602877f07189652c57b4
Guilherme <guibbs11032003@gmail.com>
2025-01-13 21:09:22 -0300

    att: cta animation

--- src/layout/banner/Banner.js
+++ src/layout/banner/Banner.js
@@ -1,23 +1,10 @@
-import { useRef } from 'react';
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
 import styles from './Banner.module.scss';
 
 export const Banner = () => {
-    const bannerNameRef = useRef(null);
-
-    useGSAP(() => {
-        gsap.fromTo(
-            bannerNameRef.current,
-            { opacity: 0 },
-            { opacity: 1, duration: 1, ease: "power1.in" }
-        );
-    }, [])
-
     return (
         <section className={styles.banner_container}>
             <div className={styles.banner_background}>
-                <div ref={bannerNameRef} className={styles.banner_name}>
+                <div className={styles.banner_name}>
                     <span className={styles.banner_name_first}>
                         Luana
                     </span>
--- src/layout/cta/Cta.js
+++ src/layout/cta/Cta.js
@@ -1,9 +1,38 @@
+import { useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import gsap from 'gsap';
+
 import styles from './Cta.module.scss';
 
+gsap.registerPlugin(ScrollTrigger);
+
 export const Cta = () => {
+    const backgroundRef = useRef(null);
+
+    useGSAP(() => {
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: backgroundRef.current,
+                start: "center 60%",
+                end: "center 60%",
+                toggleActions: "play none reverse none"
+            }
+        })
+            .from(`.${styles.cta_heading}`, {
+                y: "100vh",
+                duration: 1
+            })
+            .from(`.${styles.cta_brief}`, {
+                opacity: 0,
+                duration: 1
+            })
+
+    }, { scope: backgroundRef })
+
     return (
         <section className={styles.cta_container}>
-            <div className={styles.cta_background}>
+            <div className={styles.cta_background} ref={backgroundRef}>
                 <h1 className={`${styles.cta_heading} ${styles.cta_heading_filled}`}>
                     Design empowers experiences
                 </h1>
